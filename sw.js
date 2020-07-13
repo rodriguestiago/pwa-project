cfa2307811bdb0dbc44a878977163015c27e857e
rodriguestiago <58735023+rodriguestiago@users.noreply.github.com>
2020-07-13 16:26:38 +0200

    sw

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+      // Après l'installation du service worker,
+      // ouvre un nouveau cache
+      caches.open('mon-cache-pwa').then(cache => {
+        // Ajoute toutes les URLs des éléments à mettre en cache
+        return cache.addAll([
+          '/',
+          '/index.html'
+        ]);
+      })
+    );
+   });
